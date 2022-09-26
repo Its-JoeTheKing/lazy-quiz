e1fa0e19551ecbb7360510deb0988b30fcb5c5aa
Its-JoeTheKing <errfig22@gmail.com>
2022-09-26 22:23:41 +0100

    q

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 const generate = () => {
-  var num = Math.random(Math.floor()*14)
+  var num = Math.random()*14
   console.log(num);
 }
 generate()
